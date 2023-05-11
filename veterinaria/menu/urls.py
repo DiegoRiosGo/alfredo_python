ff180d1ead9924cf3786d0059ba81dd93c0f5cff
DiegoRiosGo <die.rios@duocuc.cl>
2023-05-11 10:20:29 -0400

    avance1

--- veterinaria/menu/urls.py
+++ veterinaria/menu/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Inicio,Nosotros
+from .views import Inicio,Nosotros,AgendaOnline,Contacto,Login,OlvidoContra,Servicios
 
 urlpatterns = [
     path('',Inicio,name="Inicio"),
     path('Nosotros/',Nosotros,name="Nosotros"),
     #----------
-    path('paginaNosotros/',Nosotros,name="Nosotros"),
+    path('AgendaOnline/',AgendaOnline,name="AgendaOnline"),
+    path('Contacto/',Contacto,name="Contacto"),
+    path('Login/',Login,name="Login"),
+    path('OlvidoContra/',OlvidoContra,name="OlvidoContra"),
+    path('Servicios/',Servicios,name="Servicios"),
 ]
