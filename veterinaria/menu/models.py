a2bdff1b3f8ec2e2fec70d1118b38545ce1a58ef
Denilxon <de.azua@duocuc.cl>
2023-05-18 10:30:36 -0400

    Tablas Base de datos(Models)

--- veterinaria/menu/models.py
+++ veterinaria/menu/models.py
@@ -3,11 +3,19 @@ from django.db import models
 # Create your models here.
 class Raza(models.Model):
     codigoRaza = models.AutoField(primary_key=True, verbose_name='Código de la Raza')
-    nombreRaza = models.CharField(max_length=20,blank=False,null=True)
+    nombreRaza = models.CharField(max_length=20,blank=False,null=True, verbose_name='Nombre de la mascota')
 
 class Mascota(models.Model):
     codigoChip = models.CharField(primary_key=True)
-    nombreMascota = models.CharField(max_length=50)
+    nombreMascota = models.CharField(max_length=50, verbose_name='Nombre de la mascota')
     edadMascota = models.IntegerField()
     foto = models.ImageField(upload_to="mascotas")
+#falta agregar la fecha de nacimiento
     raza = models.ForeignKey(Raza,on_delete=models.DO_NOTHING)
+
+class Servicios(models.Model):
+    idServicio = models.AutoField(primary_key=True, verbose_name='Codigo de los servicios')
+    nombreServicio = models.CharField(max_length=35, blank=False, null=True, verbose_name='Nombre de los servicios')
+    precioServicio = models.
+
+class
--- veterinaria/veterinaria/settings.py
+++ veterinaria/veterinaria/settings.py
@@ -131,4 +131,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = OS.path.join(BASE_DIR, 'media')
