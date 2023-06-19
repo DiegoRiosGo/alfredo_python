48a4363e5b4f8d6353a485e396dcd0a61a395420
Omar Lugo <127126941+OmarJazielLugo@users.noreply.github.com>
2023-06-18 22:46:12 -0400

    3.0

--- veterinaria/veterinaria/settings.py
+++ veterinaria/veterinaria/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'menu',
-    'rest_framework'
-    'rest_mascota'
+    'rest_framework',
+    'rest_mascota',
 ]
 
 MIDDLEWARE = [
